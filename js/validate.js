ecea5d02d4b9c338c20a5eb46430b3bdc63da45a
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2022-11-12 11:22:26 +0200

    Ensured code complies with best practices
    Added comments. Removed useless id from the <form> tag

--- js/validate.js
+++ js/validate.js
@@ -1,11 +1,16 @@
-
+//Called when form is submitted
 function validate(){
-    var newTask = document.getElementById("newTask").value;
-    console.log(newTask);
+    var newTask = document.getElementById("newTask").value; //Stores submitted newTask value
+
     if (newTask.includes("<") == true || newTask.includes(">") == true){
+        //If the newTask value contains < or > this makes the error message in the <p id="error"> tag visible
         document.getElementById("error").style.display = "block";
     }
 }
+
+//Creates list element
 var listElement = document.createElement("li");
+//Places text within the element
 listElement.innerHTML = "List";
+//Adds the element to the unordered list in the HTML document
 document.getElementById("unorderedList").appendChild(listElement);
