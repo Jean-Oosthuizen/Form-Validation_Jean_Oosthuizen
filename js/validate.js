5d9bd2b33b83c92ba611afb896e83f246de53ade
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2022-11-12 10:54:29 +0200

    Fixed the error message that wouldn't display
    When submitted with either < or >, the <p id="error"> tag wasn't changing its display style to block. This is because I forgot to use quotation marks in the Javascript that is meant to  change the style to block

--- js/validate.js
+++ js/validate.js
@@ -3,7 +3,7 @@ function validate(){
     var newTask = document.getElementById("newTask").value;
     console.log(newTask);
     if (newTask.includes("<") == true || newTask.includes(">") == true){
-        document.getElementById("error").style.display = block;
+        document.getElementById("error").style.display = "block";
     }
 }
 var listElement = document.createElement("li");
