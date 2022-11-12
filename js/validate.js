fb3bc8349eb493d2fa86dbe740ac9eae95672da6
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2022-11-12 10:39:51 +0200

    added ul, created validate function, added ul function
    I changed the CSS to only target the <p> tag with the error id instead of every <p> tag. I created the validate function, though it isn't working yet. I made the code that adds a list element to the <ul> on the page. I changed the form method and removed the names of all the inputs since they seemed to be redundant. But the form and associated code are both not working, and it seems like the page refreshes once I hit submit on the form.

---
+++ js/validate.js
@@ -0,0 +1,11 @@
+
+function validate(){
+    var newTask = document.getElementById("newTask").value;
+    console.log(newTask);
+    if (newTask.includes("<") == true || newTask.includes(">") == true){
+        document.getElementById("error").style.display = block;
+    }
+}
+var listElement = document.createElement("li");
+listElement.innerHTML = "List";
+document.getElementById("unorderedList").appendChild(listElement);
