15d9d38fffc5adacaf219604a6702e14e44c9db4
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2022-11-12 12:10:43 +0200

    Styled the page to look nicer
    Added spacing. Added colors, and went for a minimalist warm and calming color set. Placed every element within the form into a grid so that the form is tabular and even. Changed bullet point styling to an emojie.

--- js/validate.js
+++ js/validate.js
@@ -1,16 +1,14 @@
-//Called when form is submitted
+//Called when the form is submitted
 function validate(){
     var newTask = document.getElementById("newTask").value; //Stores submitted newTask value
 
     if (newTask.includes("<") == true || newTask.includes(">") == true){
-        //If the newTask value contains < or > this makes the error message in the <p id="error"> tag visible
+        //If the newTask value contains < or >, this makes the error message in the <p id="error"> tag visible
         document.getElementById("error").style.display = "block";
     }
 }
 
-//Creates list element
+//Creates list element, places text in the element, then places the element in the HTML page
 var listElement = document.createElement("li");
-//Places text within the element
 listElement.innerHTML = "List";
-//Adds the element to the unordered list in the HTML document
 document.getElementById("unorderedList").appendChild(listElement);
