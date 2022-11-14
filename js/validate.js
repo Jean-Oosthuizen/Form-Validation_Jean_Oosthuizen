441e8a398e4809009029d886aec3ce413fd1520e
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2022-11-14 08:35:58 +0200

    List creation functions properly, added success notification
    I misunderstood the task to mean that the list creation code should run when the javascript loads. It is meant to add the name of a task that is submitted, and so I moved the code to within the validate function. I also added a success message that displays when a value is successfully submitted. Made the text in both success and error messages center aligned. Changed the error message text slightly to make it feel more impassive.

--- js/validate.js
+++ js/validate.js
@@ -3,12 +3,17 @@ function validate(){
     var newTask = document.getElementById("newTask").value; //Stores submitted newTask value
 
     if (newTask.includes("<") == true || newTask.includes(">") == true){
-        //If the newTask value contains < or >, this makes the error message in the <p id="error"> tag visible
+        //If the newTask value contains < or >, displays error message, hides success message
         document.getElementById("error").style.display = "block";
+        document.getElementById("success").style.display = "none";
+    }else{
+        //If value is valid, displays success message, hides error message, creates list element with that value
+        document.getElementById("success").style.display = "block";
+        document.getElementById("error").style.display = "none";
+        var listElement = document.createElement("li");
+        listElement.innerHTML = newTask;
+        document.getElementById("unorderedList").appendChild(listElement);
     }
 }
 
 //Creates list element, places text in the element, then places the element in the HTML page
-var listElement = document.createElement("li");
-listElement.innerHTML = "List";
-document.getElementById("unorderedList").appendChild(listElement);
